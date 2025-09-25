aa31d50683d27e64ca3ea3b5b1b639a96b82ac46
ESolucoes <enzo.trabalho.cel@gmail.com>
2025-09-25 17:54:27 -0300

    Correção vercel

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,23 @@
+// api/index.ts
 import 'reflect-metadata';
 import serverless from 'serverless-http';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module'; // ⬅️ importa da SRC (não de dist)
 
 let handler: any;
+
 async function bootstrap() {
-  const { AppModule } = require('../dist/app.module');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.init();
+  // @ts-ignore
   return serverless(app.getHttpAdapter().getInstance());
 }
+
 export default async (req: any, res: any) => {
   if (!handler) handler = await bootstrap();
   return handler(req, res);
